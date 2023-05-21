b9196bfbe9d39f03a8b5776366976a3c9835a6e7
Aaditya Gagneja <aadityaarora1215@gmail.com>
2023-05-21 23:18:15 +0530

    Adding Login controller and Making all user route

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,17 @@
-exports.getAllUsers = (req, res) => {
-    res.status(500).json({
-        status: "error",
-        message: "This route has not been made yet!!!"
+const User = require('./../models/userModel')
+const catchAsync = require('./../Utils/catchAsync')
+
+exports.getAllUsers = catchAsync(async (req, res , next) => {
+    const users = await User.find() ;
+    
+    res.status(200).json({
+        status : 'sucess' , 
+        results : users.length , 
+        data : {
+            users
+        }
     })
-}
+})
 
 exports.getUser = (req, res) => {
     res.status(500).json({
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -7,6 +7,9 @@ const router = express.Router()
 router.route('/signup')
     .post(authController.signup)
 
+router.route('/login')
+    .post(authController.login)    
+
 router.route('/')
     .get(userController.getAllUsers)
     .post(userController.createUser)
