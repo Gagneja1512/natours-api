52610e72e19a126b3b795e68128c6e90f4eb6c63
Aaditya Gagneja <aadityaarora1215@gmail.com>
2023-05-25 18:31:07 +0530

    Adding some security Measures

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
-
+const helmet = require('helmet')
+const rateLimit = require('express-rate-limit')
+const mongoSanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean')
+const hpp = require('hpp')
 const AppError = require('./Utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
 const tourRouter = require('./routes/tourRouter')
@@ -8,11 +12,33 @@ const userRouter = require('./routes/userRouter')
 
 const app = express()
 
+app.use(helmet())
+
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
 }
 
+const limiter = rateLimit({
+    max : 100 ,
+    windowMs : 60*60*1000 ,
+    message : "To many requests from this IP. Request after some time"
+})
+
+app.use('/api' , limiter)
+
 app.use(express.json())
+
+// prevent No SQL query injections
+app.use(mongoSanitize()) 
+
+// Prevent Against xss attacks
+app.use(xss())
+
+// Parameter pollution
+app.use(hpp({
+    whitelist : ['duration' , 'ratingsAverage' , 'ratingsQuantity' , 'maxGroupSize' , 'price' , 'difficulty']
+}))
+
 app.use(express.static(`${__dirname}/public`))
 
 app.use((req , res , next) => {
